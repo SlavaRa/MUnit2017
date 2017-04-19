d1220c969e17c67e5d9257d95e123bccf3de1ff6
Slava Buynov <SlavaB@playtika.com>
2017-04-19 10:27:08 +0300

    Cleanup...

--- tool/src/massive/munit/command/GenerateCommand.hx
+++ tool/src/massive/munit/command/GenerateCommand.hx
@@ -31,59 +31,41 @@ import massive.haxe.util.TemplateUtil;
 import massive.munit.client.HTTPClient;
 import massive.sys.io.File;
 
-class GenerateCommand extends MUnitCommand
-{
+class GenerateCommand extends MUnitCommand {
 	private var dir:File;
 	private var hxmlOutput:File;
 	private var includeHxml:Bool;
 	private var testFilter:String;
 	
-	public function new()
-	{
+	public function new() {
 		super();
 		testFilter = null;
 		includeHxml = true;
 	}
 	
-	override public function initialise():Void
-	{
+	override public function initialise():Void {
 		var testSrcPath = console.getNextArg();
 		dir = initialiseTestSourceDirectory(testSrcPath);
 		includeHxml = console.getOption("nohxml") != "true";
-		if(!includeHxml)
-		{
+		if(!includeHxml) {
 			var hxmlPath  = console.getNextArg();
 			hxmlOutput = initialiseHxmlOutputFile(hxmlPath);
 		}
 		testFilter = console.getOption("-filter");
 	}
 
-	private function initialiseTestSourceDirectory(?testSrcPath:String=null):File
-	{
+	private function initialiseTestSourceDirectory(?testSrcPath:String = null):File {
 		var dir:File;
-		if(testSrcPath == null)
-		{
+		if(testSrcPath == null) {
 			dir = config.src;
-			if(dir == null)
-			{
-				error("Default test src directory is not set. Please run munit config.");
-			}
-			if(!dir.exists)
-			{
-				error("Default test src directory does not exist (" + config.dir.getRelativePath(dir) + ").\nPlease run munit config or specify a <src> and <hxml> path");
-			}
-		}
-		else
-		{
+			if(dir == null) error("Default test src directory is not set. Please run munit config.");
+			if(!dir.exists) error("Default test src directory does not exist (" + config.dir.getRelativePath(dir) + ").\nPlease run munit config or specify a <src> and <hxml> path");
+		} else {
 			dir = File.create(testSrcPath, console.dir);
-			if (!dir.exists)
-			{
-				try
-				{
+			if(!dir.exists) {
+				try {
 					dir.createDirectory();
-				}
-				catch(e:Dynamic)
-				{
+				} catch(e:Dynamic) {
 					error("Could not create test directory: " + e);
 				}
 			}
@@ -92,26 +74,19 @@ class GenerateCommand extends MUnitCommand
 	}
 
 	
-	private function initialiseHxmlOutputFile(?hxmlPath:String=null):File
-	{
+	private function initialiseHxmlOutputFile(?hxmlPath:String = null):File {
 		var hxmlOutput:File = null;
-		if(hxmlPath == null) 
-		{
-			if( config.hxml == null)	error("Default hxml path is not set. Please run munit config or specify a hxml file path.");
+		if(hxmlPath == null) {
+			if(config.hxml == null) error("Default hxml path is not set. Please run munit config or specify a hxml file path.");
 			else hxmlOutput =  config.hxml;
 		}
-		else
-		{
-			hxmlOutput = File.create(hxmlPath, config.dir); 
-		}
+		else hxmlOutput = File.create(hxmlPath, config.dir); 
 		if(hxmlOutput == null || !hxmlOutput.isFile) error("Invalid hxml path " + hxmlPath);
 		return hxmlOutput;
 	}
 	
-	override public function execute():Void
-	{
-		if(!hasTestMain())
-		{
+	override public function execute():Void {
+		if(!hasTestMain()) {
 			creatTestMainClass();
 			createExampleTestClass();
 			if(includeHxml) createTestHxmlFile();
@@ -119,57 +94,43 @@ class GenerateCommand extends MUnitCommand
 		createTestSuiteClass();
 	}
 	
-	private function hasTestMain():Bool
-	{
-		var testMain:File = dir.resolvePath("TestMain.hx");
-		return testMain.exists;
-	}
+	private function hasTestMain():Bool return dir.resolvePath("TestMain.hx").exists;
 	
-	private function creatTestMainClass():Void
-	{
+	private function creatTestMainClass():Void {
 		var testMain:File = dir.resolvePath("TestMain.hx");
 		var content = TemplateUtil.getTemplate("test-main", {url:HTTPClient.DEFAULT_SERVER_URL});
 		testMain.writeString(content, true);
 	}
 	
-	private function createExampleTestClass():Void
-	{
+	private function createExampleTestClass():Void {
 		//create an example test class for reference
 		var testExample:File = dir.resolvePath("ExampleTest.hx");
 		var content = TemplateUtil.getTemplate("test-example");
 		testExample.writeString(content, true);
 	}
 	
-	private function createTestHxmlFile():Void
-	{
+	private function createTestHxmlFile():Void {
 		if(hxmlOutput == null) return;
 		if(hxmlOutput.exists) return;
-			
 		var src:String = config.src != null ? config.dir.getRelativePath(config.src) + "" : "";
 		var bin:String = config.bin != null ? config.dir.getRelativePath(config.bin) + "" : "";
-
 		//write out stub hxml file
 		var content = TemplateUtil.getTemplate("test-hxml", {src:src, bin:bin});
 		hxmlOutput.writeString(content, true);
 	}
 	
-	private function createTestSuiteClass():Void
-	{
+	private function createTestSuiteClass():Void {
 		var classes:Array<String> = getFilteredClassesInDirectory(dir);
 		var content:String = generateTestSuiteClassFromClasses(classes);
-
 		var testSuite:File = dir.resolvePath("TestSuite.hx");
 		testSuite.writeString(content, true);
 	}	
 	
-	private function getFilteredClassesInDirectory(dir:File):Array<String>
-	{
+	private function getFilteredClassesInDirectory(dir:File):Array<String> {
 		var files:Array<File> = dir.getRecursiveDirectoryListing(~/.*Test\.hx$/);
 		var classes:Array<String> = [];
-		var clasz:String;
-		for(file in files)
-		{
-			clasz = dir.getRelativePath(file).substr(0, -3);
+		for(file in files) {
+			var clasz:String = dir.getRelativePath(file).substr(0, -3);
 			clasz = clasz.split("/").join(".");
 			if(clasz == "TestMain") continue;
 			if(testFilter != null && clasz.indexOf(testFilter) == -1) continue;
@@ -179,12 +140,10 @@ class GenerateCommand extends MUnitCommand
 		return classes;
 	}
 	
-	private function generateTestSuiteClassFromClasses(classes:Array<String>):String
-	{
+	private function generateTestSuiteClassFromClasses(classes:Array<String>):String {
 		var imports:String = "";
 		var tests:String = "";
-		for(clasz in classes)
-		{
+		for(clasz in classes) {
 			imports += "\nimport " + clasz + ";";
 			tests += "\n		add(" + clasz + ");";
 		}
